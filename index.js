4e36c3b8c4f04fb7e7360b219aff01c1bc9093a9
Ste Coleman <stecoleman1@gmail.com>
2020-10-30 08:09:19 +0000

    circle logo update
    circle logo code now fully functional and spins on scroll

--- index.js
+++ index.js
@@ -8,11 +8,21 @@
 // }
 
 
-
-
 window.addEventListener("scroll", function() {
-    document.body.style.setProperty("--scroll",window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-    );
-  },
-  false
+  document.body.style.setProperty("--scroll",window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+  );
+},
+false
 );
+
+
+var circleLogo = document.getElementById('circleLogo');
+var topCont = document.getElementsByClassName('top-container');
+
+
+// function logoMovement() {
+//   if (topCont.visible(detectPartial)){
+//     circleLogo.removetAttribute("style", "position: fixed");
+//   }
+// }
+
