a9bcd5432cb326b9e229353076bbda239db3f48f
Ste Coleman <stecoleman1@gmail.com>
2020-11-06 15:20:16 +0000

    styling edits

--- index.js
+++ index.js
@@ -14,15 +14,3 @@ window.addEventListener("scroll", function() {
 },
 false
 );
-
-
-var circleLogo = document.getElementById('circleLogo');
-var topCont = document.getElementsByClassName('top-container');
-
-
-// function logoMovement() {
-//   if (topCont.visible(detectPartial)){
-//     circleLogo.removetAttribute("style", "position: fixed");
-//   }
-// }
-
